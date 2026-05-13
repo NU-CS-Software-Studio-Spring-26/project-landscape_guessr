81470337c67840c4a23496ea232a6e218c237281
Leyla <lyatifiva.leyla@gmail.com>
2026-05-13 08:58:28 -0500

    Add 'how scoring works' page

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -5,4 +5,16 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
+
+  test "should get scoring while signed out" do
+    get scoring_url
+    assert_response :success
+    assert_select "h1", text: /How scoring works/i
+  end
+
+  test "should get scoring while signed in" do
+    sign_in_as users(:alice)
+    get scoring_url
+    assert_response :success
+  end
 end
